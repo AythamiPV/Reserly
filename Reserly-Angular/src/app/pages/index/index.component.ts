391f73568e0fa88f7a820684e956cb8144972968
Aythami <aypeve@gmail.com>
2025-05-04 17:13:27 +0100

    - Se ha creado la pagina de Manage company. - Se ha creado la pagina de Comapny website.

--- Reserly-Angular/src/app/pages/index/index.component.ts
+++ Reserly-Angular/src/app/pages/index/index.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import {FooterComponent} from '../../components/footer/footer.component';
+import {LoggedHeaderComponent} from '../../components/logged-header/logged-header.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent, LoggedHeaderComponent],
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
